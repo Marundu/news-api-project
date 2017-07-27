f1fbabda20f70d47d760e12e4d6e7f40c5d5ae5b
Marundu <marundu@gmail.com>
2017-07-27 10:14:30 +0300

    Removed verbose code, added style improvements

--- news.py
+++ news.py
@@ -1,4 +1,5 @@
 import json
+import random
 import requests
 
 from flask import Flask
@@ -9,34 +10,40 @@ app=Flask(__name__)
 
 api_key='b24ad8f28e1b4e089ee9bca096f11012'
 
-aj='al-jazeera-english'
-arst='ars-technica'
-bi='business-insider'
-bf='buzzfeed'
-eng='engadget'
-ft='financial-times'
-hn='hacker-news'
-mash='mashable'
-natgeo='national-geographic'
-pg='polygon'
-rc='recode'
-rt='reuters'
-tc='techcrunch'
-tr='techradar'
-econ='the-economist'
-tguk='the-guardian-uk'
-tnw='the-next-web'
-nyt='the-new-york-times'
-tv='the-verge'
+sources_list=[
+    'al-jazeera-english', 
+    'ars-technica', 
+    'business-insider', 
+    'buzzfeed', 'engadget', 
+    'financial-times', 
+    'hacker-news', 
+    'mashable', 
+    'national-geographic', 
+    'polygon', 
+    'recode', 
+    'reuters', 
+    'techcrunch', 
+    'techradar', 
+    'the-economist', 
+    'the-guardian-uk', 
+    'the-next-web', 
+    'the-new-york-times', 
+    'the-verge'
+    ]
 
-base_url='https://newsapi.org/v1/articles?source={}&sortBy=latest&apiKey='.format(eng)
+for i in range(len(sources_list)):
+    random_sources_list=random.shuffle(sources_list)
+    source=random.choice(sources_list)
+
+base_url='https://newsapi.org/v1/articles?source={}&sortBy=latest&apiKey='.format(source)
 
 @app.route('/')
 def articles():
-	articles=requests.get(base_url+api_key)
-	parsed=articles.json()
-	
-	return render_template('articles.html', source=parsed['source'], articles=parsed['articles'])
+    articles=requests.get(base_url+api_key)
+    parsed=articles.json()
+    source=parsed['source']
+    
+    return render_template('articles.html', source=source, articles=parsed['articles'])
 
 if __name__=='__main__':
-	app.run(port=7100, debug=True)
+    app.run(port=7100, debug=True)
