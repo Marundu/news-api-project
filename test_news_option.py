a8c1883c6dedf6e0339ae2017d814260aca6c051
Marundu <marundu@gmail.com>
2017-09-26 12:01:32 +0300

    Syntax and cosmetic fixes

---
+++ test_news_option.py
@@ -0,0 +1,57 @@
+import json
+import random
+import requests
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app=Flask(__name__)
+
+api_key='b24ad8f28e1b4e089ee9bca096f11012'
+
+sources=[
+'al-jazeera-english', 
+'ars-technica', 
+'business-insider', 
+'buzzfeed', 
+'engadget', 
+'financial-times', 
+'hacker-news', 
+'mashable', 
+'national-geographic', 
+'polygon', 
+'recode', 
+'reuters', 
+'techcrunch', 
+'techradar', 
+'the-economist', 
+'the-guardian-uk', 
+'the-next-web', 
+'the-new-york-times', 
+'the-verge',
+'the-huffington-post',
+'time',
+'the-wall-street-journal',
+'the-times-of-india',
+'reddit-r-all',
+'newsweek',
+'new-scientist',
+'fortune',
+'cnn',
+]
+
+@app.route('/', methods=['GET','POST'])
+def articles():
+    results=[]
+    for source in sources:
+        newsapi_url='https://newsapi.org/v1/articles?source={}&sortBy=latest&apiKey='.format(source)
+        articles=requests.get(newsapi_url+api_key)
+        parsed=articles.json()
+        if 'source' in parsed and 'articles' in parsed:
+            results.append(parsed)
+    return render_template('test_articles.html', results=results)    
+
+
+if __name__=='__main__':
+    app.run(port=7100, debug=True)
